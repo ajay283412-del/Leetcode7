d321514ece4210e5d1bab47b921f44b13be9f6d8
ajay283412-del <ajay283412@gmail.com>
2026-06-24 04:48:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.5 MB (24.34%)

---
+++ 0874-backspace-string-compare/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        l1=[]
+        l2=[]
+        for i in s:
+            if i=='#' and len(l1)>0:
+                l1.pop()
+            elif i!='#':
+                l1.append(i) 
+        for i in t:
+            if i=='#' and len(l2)>0:
+                l2.pop()
+            elif i!='#':
+                l2.append(i)
+        return l1==l2
