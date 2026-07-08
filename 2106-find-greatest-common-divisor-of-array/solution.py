84d67fba4f0273d27b73417433c2d214e9ab1f8a
ajay283412-del <ajay283412@gmail.com>
2026-07-08 06:32:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.5 MB (11.24%)

--- 2106-find-greatest-common-divisor-of-array/solution.py
+++ 2106-find-greatest-common-divisor-of-array/solution.py
@@ -2,5 +2,4 @@ class Solution:
     def findGCD(self, nums: List[int]) -> int:
         min1=min(nums)
         max1=max(nums)
-        return math.gcd(min1,max1)
-        
+        return gcd(min1,max1)
